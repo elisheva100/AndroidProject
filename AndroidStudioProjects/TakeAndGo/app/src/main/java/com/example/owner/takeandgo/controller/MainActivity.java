1400a45369aeb0684a3a412cc5e2ff5d022e3fd9
TairCohen <tairtif@gmail.com>
2017-11-09 22:13:15 +0200

    nefvfc

--- AndroidStudioProjects/TakeAndGo/app/src/main/java/com/example/owner/takeandgo/controller/MainActivity.java
+++ AndroidStudioProjects/TakeAndGo/app/src/main/java/com/example/owner/takeandgo/controller/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.owner.takeandgo;
+package controller;
 
 import android.app.Activity;
 import android.os.Bundle;
+
+import com.example.owner.takeandgo.R;
 
 public class MainActivity extends Activity {
 
