060b8b2fca042a1ede05e16107a40eb2cf54f562
TairCohen <tairtif@gmail.com>
2017-12-06 20:52:33 +0200

    new show list

--- AndroidStudioProjects/TakeAndGo/app/src/main/java/com/example/owner/takeandgo/controller/ShowCarListActivity.java
+++ AndroidStudioProjects/TakeAndGo/app/src/main/java/com/example/owner/takeandgo/controller/ShowCarListActivity.java
@@ -1,6 +1,7 @@
 package com.example.owner.takeandgo.controller;
 //TODO add Asynig task
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +13,7 @@ import com.example.owner.takeandgo.R;
 import com.example.owner.takeandgo.model.backEnd.DBManagerFactory;
 import com.example.owner.takeandgo.model.entities.Car;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ShowCarListActivity extends Activity {
@@ -24,28 +26,44 @@ public class ShowCarListActivity extends Activity {
 
     void initItemByListView()
     {
-        ListView listView = new ListView(this);
-        final List<Car> myItemList = DBManagerFactory.getManager().getCars();
-        ArrayAdapter<Car> adapter = new ArrayAdapter<Car>(this, R.layout.activity_show_car_list, myItemList )
-        {
+        final ListView listView = new ListView(this);
+        new AsyncTask<Void, Void, List<Car>>() {
             @Override
-            public View getView(int position, View convertView, ViewGroup parent)
+            protected  void onPostExecute(final List<Car> myItemList)
             {
-                if (convertView == null) {
-                    convertView = View.inflate(ShowCarListActivity.this, R.layout.activity_show_car_list, null);
-                }
-                TextView productNumberTextView = (TextView) convertView.findViewById(R.id.numberTextView);
-                TextView productBranchNumberTextView = (TextView) convertView.findViewById(R.id.branchNumberTextView);
-                TextView productionModelTypeTextView = (TextView) convertView.findViewById(R.id.modelTypeTextView);
-                TextView productionMileageTextView = (TextView) convertView.findViewById(R.id.mileageTextView);
-                productNumberTextView.setText((int) myItemList.get(position).getNumber());
-                productBranchNumberTextView.setText(( myItemList.get(position).getBranchNumber()));
-                productionModelTypeTextView.setText((myItemList.get(position).getModelType()));
-                productionMileageTextView.setText((int) myItemList.get(position).getMileage());
-                return convertView;
+                ArrayAdapter<Car> adapter = new ArrayAdapter<Car>(ShowCarListActivity.this, R.layout.activity_show_car_list,myItemList)
+                {
+                    @Override
+                    public View getView(int position, View convertView, ViewGroup parent)
+                    {
+                        if (convertView == null) {
+                            convertView = View.inflate(ShowCarListActivity.this, R.layout.activity_show_car_list, null);
+                        }
+                        TextView productNumberTextView = (TextView) convertView.findViewById(R.id.numberTextView);
+                        TextView productBranchNumberTextView = (TextView) convertView.findViewById(R.id.branchNumberTextView);
+                        TextView productionModelTypeTextView = (TextView) convertView.findViewById(R.id.modelTypeTextView);
+                        TextView productionMileageTextView = (TextView) convertView.findViewById(R.id.mileageTextView);
+                        productNumberTextView.setText((int) myItemList.get(position).getNumber());
+                        productBranchNumberTextView.setText(( myItemList.get(position).getBranchNumber()));
+                        productionModelTypeTextView.setText((myItemList.get(position).getModelType()));
+                        productionMileageTextView.setText((int) myItemList.get(position).getMileage());
+                        return convertView;
+                    }
+                };
+                listView.setAdapter(adapter);
             }
-        };
-        listView.setAdapter(adapter);
+            @Override
+            protected List<Car> doInBackground(Void... params) {
+                try {
+                     return DBManagerFactory.getManager().getCars();
+                    }
+                catch (Exception e) {
+                    e.printStackTrace();
+                    return null;
+                }
+            }
+        }.execute();
+
         this.setContentView(listView);
     }
 }
--- AndroidStudioProjects/TakeAndGo/app/src/main/java/com/example/owner/takeandgo/controller/ShowClientListActivity.java
+++ AndroidStudioProjects/TakeAndGo/app/src/main/java/com/example/owner/takeandgo/controller/ShowClientListActivity.java
@@ -2,6 +2,7 @@ package com.example.owner.takeandgo.controller;
 //TODO add Asynig task
 import android.app.Activity;
 import android.content.ClipData;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,28 +26,42 @@ public class ShowClientListActivity extends Activity {
 
     void initItemByListView()
     {
-        ListView listView = new ListView(this);
-        final List<Client> myItemList = DBManagerFactory.getManager().getClients();
-        ArrayAdapter<Client> adapter = new ArrayAdapter<Client>(this, R.layout.activity_show_client_list, myItemList )
-        {
+        final ListView listView = new ListView(this);
+        //final List<Client> myItemList = DBManagerFactory.getManager().getClients();
+        new AsyncTask<Void, Void, List<Client>>() {
             @Override
-            public View getView(int position, View convertView, ViewGroup parent)
-            {
-                if (convertView == null) {
-                    convertView = View.inflate(ShowClientListActivity.this, R.layout.activity_show_client_list, null);
-                }
-                TextView productIdTextView = (TextView) convertView.findViewById(R.id.idTextView);
-                TextView productNameTextView = (TextView) convertView.findViewById(R.id.nameTextView);
-                TextView productionCellphoneNumberTextView = (TextView) convertView.findViewById(R.id.cellphoneNumberTextView);
-                TextView productionEmailTextView = (TextView) convertView.findViewById(R.id.emailTextView);
-                productIdTextView.setText((myItemList.get(position).getId()));
-                productNameTextView.setText(myItemList.get(position).getFirstName() + " " + myItemList.get(position).getLastName());
-                productionCellphoneNumberTextView.setText((myItemList.get(position).getCellphoneNumber()));
-                productionEmailTextView.setText((myItemList.get(position).getEmail()));
-                return convertView;
+            protected void onPostExecute(final List<Client> myItemList) {
+                final ArrayAdapter<Client> adapter = new ArrayAdapter<Client>(ShowClientListActivity.this, R.layout.activity_show_client_list, myItemList) {
+                    @Override
+                    public View getView(int position, View convertView, ViewGroup parent) {
+                        if (convertView == null) {
+                            convertView = View.inflate(ShowClientListActivity.this, R.layout.activity_show_client_list, null);
+                        }
+                        TextView productIdTextView = (TextView) convertView.findViewById(R.id.idTextView);
+                        TextView productNameTextView = (TextView) convertView.findViewById(R.id.nameTextView);
+                        TextView productionCellphoneNumberTextView = (TextView) convertView.findViewById(R.id.cellphoneNumberTextView);
+                        TextView productionEmailTextView = (TextView) convertView.findViewById(R.id.emailTextView);
+                        productIdTextView.setText((myItemList.get(position).getId()));
+                        productNameTextView.setText(myItemList.get(position).getFirstName() + " " + myItemList.get(position).getLastName());
+                        productionCellphoneNumberTextView.setText((myItemList.get(position).getCellphoneNumber()));
+                        productionEmailTextView.setText((myItemList.get(position).getEmail()));
+                        return convertView;
+                    }
+                };
+                listView.setAdapter(adapter);
             }
-        };
-        listView.setAdapter(adapter);
+
+            @Override
+            protected List<Client> doInBackground(Void... params) {
+                try {
+                    return DBManagerFactory.getManager().getClients();
+                }
+                catch (Exception e) {
+                    e.printStackTrace();
+                    return null;
+                }
+            }
+        }.execute();
         this.setContentView(listView);
     }
 }
